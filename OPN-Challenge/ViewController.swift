021380a44b724ae8e132e080c249431ce7f3ef3b
phakkharachate.j <phakkharachate.j@arise.tech>
2024-05-12 02:05:08 +0700

    Initial Commit

---
+++ OPN-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OPN-Challenge
+//
+//  Created by A667209 A667209 on 12/5/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
